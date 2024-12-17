25ee2db3f98ee7bfec03f19ca681d444474b0e51
Todd Teigland <toddteigland@gmail.com>
2024-12-17 12:38:56 -0800

    course list update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,17 @@
 # Clear existing data (if desired)
-# Round.destroy_all
-
+Round.destroy_all
 
 # Find or create rounds with the initial placeholder courses
 ["Fairmont Chateau Whistler GC", "TBD Course 2", "TBD Course 3", "TBD Course 4", "TBD Course 5", "TBD Course 6"].each_with_index do |course_name, index|
-  round = Round.find_or_create_by!(round_number: index + 1, course: course_name)
+  round = Round.find_or_create_by!(round_number: index + 1) # Match only on round_number
 
-  # Update any placeholder course names to actual course names as needed
-  if round.course == "TBD Course 2"
+  # Update course names for specific rounds
+  case round.round_number
+  when 2
     round.update(course: "Georgia Golf Club")
+  when 3
+    round.update(course: "Gold Mountain Olympic")
+  else
+    round.update(course: course_name) # Set initial course names for other rounds
   end
 end
