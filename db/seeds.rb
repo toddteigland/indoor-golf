5aaf77db57592ad1e0cd688d9d8a71c335f8383d
Todd Teigland <toddteigland@gmail.com>
2025-02-19 13:55:51 -0800

    another seed update, for course list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,16 @@
-# Clear existing data (if desired)
-# Round.destroy_all
-
 # Find or create rounds with the initial placeholder courses
 ["Fairmont Chateau Whistler GC", "TBD Course 2", "TBD Course 3", "TBD Course 4", "TBD Course 5", "TBD Course 6"].each_with_index do |course_name, index|
-  # Find the round by its unique round_number
   round = Round.find_or_initialize_by(round_number: index + 1)
 
-  # Update the course name only if it matches a placeholder or needs correction
-  case round.round_number
-  when 2
-    round.course = "Georgia Golf Club"
-  when 3
-    round.course = "Gold Mountain Olympic"
-  when 4
-    round.course = "Bear Mountain"
-  else
-    round.course = course_name if round.course.blank? || round.course.start_with?("TBD")
+  new_course_name = case round.round_number
+  when 2 then "Georgia Golf Club"
+  when 3 then "Gold Mountain Olympic"
+  when 4 then "Bear Mountain"
+  else course_name
   end
-
-  round.save! # Save changes or create new record
-end
+  
+  # Only update if the course name has changed
+  if round.course != new_course_name
+    round.update!(course: new_course_name)
+  end
 
